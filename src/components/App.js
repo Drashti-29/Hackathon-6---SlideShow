b52a7268e520099292ea2490f4b0924bb3f8a740
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-11-21 19:08:33 +0530

    slideshow

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,42 @@
-// import React, {Component, useState} from "react";
-// import '../styles/App.css';
-
-// const App = () => {
-//   return (
-//     <></>
-//   )
-// }
-
-// export default App;
-
 import React, { Component, useState } from "react";
-import "../styles/App.css";
-// import slides from "./data.js"
+// import "../styles/App.css";
+import slides from "./data.js";
 const App = () => {
   const [displayimage, setimage] = useState(slides[0].title);
+  const [displaytext, settext] = useState(slides[0].text);
   const handleReset = () => {
     setimage(slides[0].title);
+    settext(slides[0].text);
   };
   const handleprev = (title) => {
-    console.log("prev");
+    let copydata = [...slides];
+    copydata.map((el) => {
+      if (el.title === title) {
+        let a = copydata.indexOf(el.title);
+        //    console.log(a);
+        if (a > -1) {
+          --a;
+          setimage(copydata[a].title);
+          settext(copydata[a].text);
+        }
+      }
+      return copydata;
+    });
+  };
+  const handlenext = (title) => {
+    let copydata = [...slides];
+    copydata.map((el) => {
+      if (el.title === title) {
+        let a = copydata.indexOf(el.title);
+        if (a < copydata.length - 2) {
+          let b = a + 1;
+          //  console.log(b);
+          setimage(copydata[b].title);
+          settext(copydata[b].text);
+        }
+      }
+      return copydata;
+    });
   };
   return (
     <>
@@ -31,8 +49,14 @@ const App = () => {
       >
         previous
       </button>
-      <button data-testid="button-next">next</button>
-      <div>{displayimage}</div>
+      <button
+        data-testid="button-next"
+        onClick={() => handlenext(displayimage)}
+      >
+        next
+      </button>
+      <h1 data-testid="title">{displayimage}</h1>
+      <p data-testid="text">{displaytext}</p>
       {/* <slides/> */}
     </>
   );
