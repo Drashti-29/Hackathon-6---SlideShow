352321b69944a263b59751bde6f830eb35d3ca33
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-11-21 18:27:29 +0530

    slideshow

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { Component, useState } from "react";
+// import "../styles/App.css";
+import slides from "./data.js";
 const App = () => {
+  const [displayimage, setimage] = useState(slides[0].title);
+  const handleReset = () => {
+    setimage(slides[0].title);
+  };
+  const handleprev = (title) => {
+    console.log("prev");
+  };
   return (
-    <></>
-  )
-}
-
+    <>
+      <button data-testid="button-restart" onClick={handleReset}>
+        reset
+      </button>
+      <button
+        data-testid="button-prev"
+        onClick={() => handleprev(displayimage)}
+      >
+        previous
+      </button>
+      <button data-testid="button-next">next</button>
+      <div>{displayimage}</div>
+      {/* <slides/> */}
+    </>
+  );
+};
 
 export default App;
